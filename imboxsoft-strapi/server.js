c4ed0d69ec87387eebad2a925b185f56ac0fcbe7
bojiix <bojanalex35@yahoo.com>
2025-02-16 16:12:23 +0200

    erg

--- imboxsoft-strapi/server.js
+++ imboxsoft-strapi/server.js
@@ -2,11 +2,5 @@ const strapi = require("@strapi/strapi");
 strapi
     .createStrapi({
         distDir: "./dist",
-        env: "production",
-        host: "0.0.0.0",
-        port: 1337,
-        logger: {
-            level: "info",
-        },
     })
     .start();
